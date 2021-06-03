4ae9197445efd557443fdf0749f7a479a91b77b8
AdlerCachuba <adlercachuba@gmail.com>
2021-06-03 10:45:49 -0300

    Implementaçcao da sessao

---
+++ app/Http/Controllers/SecaoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Secao;
+use Illuminate\Http\Request;
+
+class SecaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    //mostrar todos
+    {
+        $secao = Secao::all();
+        return response()->json($secao);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //cria nova secao
+        $secao = new Secao();
+        $secao->fill($request->all());
+        $secao->save();
+        //retorna o json de 201, foi criado.
+        return response()->json($secao,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Secao  $secao
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Secao $secao)
+    {
+        return response()->json($secao,200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Secao  $secao
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // o laravel usa a secao, e pega o id, por injeção de dependência
+    public function update(Request $request, Secao $secao){
+        // request é o body da requisição que tá chegando
+        $dadosDaSecao = $request->all();
+
+        //atribui o nome/status a secao
+        $secao->fill($dadosDaSecao);
+
+        //o save faz update/insert, mas e já existe, ele atualiza.
+        $secao->save();
+
+        //retorna o json de 200, foi atualizado.
+        return response()->json($secao,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Secao  $secao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Secao $secao)
+    {
+        $secao->delete();
+        return response()->noContent(204);
+        //retorna 204, porque não tem nada para voltar.
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SecaoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('secao')->group(function (){
+
+    //Pegar todas as secoes
+    Route::get('/', [SecaoController::class, 'index']);
+
+    Route::get('/{secao}',[SecaoController::class, 'show']);
+
+    Route::post('/',[SecaoController::class, 'store']);
+
+    Route::put('/{secao}',[SecaoController::class, 'update']);
+
+    Route::delete('/{secao}',[SecaoController::class, 'destroy']);
+
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
